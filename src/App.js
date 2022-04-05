a1826589f5aa046a914c0f70886c7789bcf76d49
unknown <izacaqsha@gmail.com>
2022-04-05 14:20:19 +0700

    initial web routes are done, next task is to start creating a basic home layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+import WebRoutes from "./routes/WebRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <WebRoutes />
+    </>
   );
 }
 
--- src/routes/WebRoutes.jsx
+++ src/routes/WebRoutes.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/home/Home";
+import Detail from "../pages/detail/Detail";
+import Register from "../pages/register/Register";
+import SignIn from "../pages/signIn/SignIn";
+import Profile from "../pages/profile/Profile";
+import Cart from "../pages/cart/Cart";
+import Order from "../pages/order/Order";
+import History from "../pages/history/History";
 
 export default function WebRoutes() {
-  return <div>WebRoutes</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
