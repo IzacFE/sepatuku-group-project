26c98e90f858fe03887530d5faafbf82ace52377
destiawanD <destiawanyk@gmail.com>
2022-04-07 21:35:10 +0700

    add order page not fix yet

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.baseURL = "52.87.250.27:8080/api/v1";
 
 ReactDOM.render(<App />, document.getElementById("root"));
