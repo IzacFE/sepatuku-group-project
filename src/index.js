e36e4cf7df4a769f576148763824245cb4d3bdec
unknown <izacaqsha@gmail.com>
2022-04-08 08:40:22 +0700

    discussion about how to setting the sign in page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import axios from "axios";
 
 axios.defaults.baseURL = "http://52.87.250.27:8080/api/v1";
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import "./SignIn.css"
+import React, { useState } from "react";
+import "./SignIn.css";
 
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -7,39 +7,58 @@ import axios from "axios";
 export default function SignIn() {
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignUp = async (e) => {
     e.preventDefault();
     await axios
-    .post("2.87.250.27:8080/api/v1/users",
-    {
-        username: 'username',
-        email: 'email',
-        password: 'password',
+      .post("/auth", {
+        username: "username",
+        email: "email",
+        password: "password",
       })
       .then((response) => {
-        console.log(response.data)
+        console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
-      })
-    };
-  
+      });
+  };
+
   return (
     <div className="signinWrapper">
       <div className="signinBox">
         <div className="signinContent">
           <h2>Masuk</h2>
           <p>Masuk untuk berbelanja</p>
-          <form onSubmit={(e) =>handleSignUp(e)}>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username Anda" />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Kata Sandi Anda" />
+          <form onSubmit={(e) => handleSignUp(e)}>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username Anda"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Kata Sandi Anda"
+            />
             <button>Masuk</button>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+/* 
+pertama post username dan email ke api, nanti responsenya kalau berhasil maka akan mendapatkan sekumpulan data dalam objek, kamu ambil token terus masukan ke local storage
+
+  localStorage.setItem(
+                    "detail",
+                    JSON.stringify(response.data.token)
+                  );
+
+ */
